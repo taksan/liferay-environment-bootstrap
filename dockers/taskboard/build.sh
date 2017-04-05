8c67a672e21280d0cba9ed507cc7e5baea223211
takeuchi <g.takeuchi@gmail.com>
2017-04-05 07:44:49 -0300

    [GSSDLC-59] new taskboard version

--- dockers/taskboard/build.sh
+++ dockers/taskboard/build.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-TASKBOARD_VERSION=0.0.3
+TASKBOARD_VERSION=0.0.4
 
 if [[ ! -e taskboard.war ]]; then
 	curl -o taskboard.war http://repo:8080/archiva/repository/internal/br/com/objective/taskboard/${TASKBOARD_VERSION}/taskboard-${TASKBOARD_VERSION}.war
