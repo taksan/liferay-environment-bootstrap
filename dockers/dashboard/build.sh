f8f7c570df852db45815f77e2141c8af7e170357
takeuchi <g.takeuchi@gmail.com>
2017-04-17 14:36:54 -0300

    GSSDLC-59 credentials do clone without ssh key

--- dockers/dashboard/build.sh
+++ dockers/dashboard/build.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-git clone git@gitlab:devops/liferay_dashing.git
+git clone http://builder:rhdes@gitlab/devops/liferay_dashing.git
 rm -rf liferay_dashing/.git
 tar czf liferay_dashing.tar.gz liferay_dashing
 sudo docker build --no-cache=true -t 'liferay-smashing' .
