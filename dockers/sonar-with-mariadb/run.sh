2d3a73e5e5e0f0e9bd2b61c67a66bff5ebfe104c
takeuchi <g.takeuchi@gmail.com>
2017-04-19 14:33:15 -0300

    GSSDLC-59 sonar mariadb experiment

--- dockers/sonar-with-mariadb/run.sh
+++ dockers/sonar-with-mariadb/run.sh
@@ -40,7 +40,7 @@ function readpassword()
 			echo "Passwords don't match"
 		fi  
 	done
-    eval "$1=$PASSWORD"exec 
+    eval "$1=$PASSWORD"
 }
 
 SONAR_PASS_FILE=$SONARQUBE_HOME/data/sonar.password 
@@ -76,7 +76,7 @@ else
 fi 
 
 
-SONARQUBE_JDBC_URL=jdbc:mysql://localhost:3306/sonar?useUnicode=true&characterEncoding=utf8&rewriteBatchedStatements=true
+export SONARQUBE_JDBC_URL="jdbc:mysql://localhost:3306/sonar?useUnicode=true&characterEncoding=utf8&rewriteBatchedStatements=true"
 
 cd $SONARQUBE_HOME
 FIRST_TIME=false
@@ -87,7 +87,14 @@ if [[ ! -e $SONAR_PASS_FILE ]]; then
     echo "## Type sonar user's password (user will be sonar)"
     readpassword SONAR_PASSWORD sonar
 
-    echo "CREATE USER 'sonar'@'%' IDENTIFIED BY '$SONAR_PASSWORD' ;" | "${mysql[@]}"
+    cat <<EOF  | "${mysql[@]}"
+    CREATE DATABASE sonar CHARACTER SET utf8 COLLATE utf8_general_ci;
+    CREATE USER 'sonar'@'%' IDENTIFIED BY '$SONAR_PASSWORD' ;
+    GRANT ALL ON sonar.* TO 'sonar'@'%' IDENTIFIED BY 'sonar';
+    GRANT ALL ON sonar.* TO 'sonar'@'localhost' IDENTIFIED BY 'sonar';
+    FLUSH PRIVILEGES;
+EOF
+
     echo $SONAR_PASSWORD > $SONAR_PASS_FILE
 fi
 LOG_FILE=$SONARQUBE_HOME/data/sonar.log
@@ -96,6 +103,14 @@ SONAR_PASSWORD=$(cat $SONAR_PASS_FILE)
 if [[ -e $LOG_FILE ]]; then
     mv $LOG_FILE $LOG_FILE.$(date +%Y%m%d%H%M%S)
 fi
+
+echo java -jar lib/sonar-application-$SONAR_VERSION.jar \
+  -Dsonar.log.console=true \
+  -Dsonar.jdbc.username="sonar" \
+  -Dsonar.jdbc.password="$SONAR_PASSWORD" \
+  -Dsonar.jdbc.url="$SONARQUBE_JDBC_URL" \
+  -Dsonar.web.javaAdditionalOpts="$SONARQUBE_WEB_JVM_OPTS -Djava.security.egd=file:/dev/./urandom" \
+  "$@" 
 
 java -jar lib/sonar-application-$SONAR_VERSION.jar \
   -Dsonar.log.console=true \
--- dockers/sonar-with-mariadb/start.sh
+++ dockers/sonar-with-mariadb/start.sh
@@ -1,4 +1,3 @@
-mkdir -p /opt/sonarmdb-data /opt/sonarmdb-mariadb
 #chcon -Rt svirt_sandbox_file_t /opt/sonar-data /opt/sonar-mysql
 docker rm lf-sonar-mdb
-docker run -p 9000:9000 --name lf-sonar-mdb -p 3306:3306 -v /opt/sonarmdb-data:/opt/sonarqube/data  -v /opt/sonarmdb-mariadb:/var/lib/mysql --rm -it devopsobj/sonar-with-mariadatabase
+docker run -p 9000:9000 --name lf-sonar-mdb -p 3306:3306 -v /opt/sonar-data:/opt/sonarqube/data  -v /opt/sonar-mariadb:/var/lib/mysql --rm -it devopsobj/sonar-with-mariadatabase
